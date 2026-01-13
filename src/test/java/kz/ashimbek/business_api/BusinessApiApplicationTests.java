ad1ca1ebc36d9f9489e5a2608e5040087015c909
Elitrois <ashimbek.tz@gmail.com>
2026-01-13 14:03:22 +0500

    First commit

---
+++ src/test/java/kz/ashimbek/business_api/BusinessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.ashimbek.business_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
